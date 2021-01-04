5c2be537aa471ad64ba3ed1b293bce47a18fa09a
Johan Vos <johan@lodgon.com>
2021-01-04 20:22:25 +0100

    add more buffer logic

--- rt/emul/compact/src/main/java/java/nio/ByteBuffer.java
+++ rt/emul/compact/src/main/java/java/nio/ByteBuffer.java
@@ -4,6 +4,9 @@ public class ByteBuffer extends Buffer {
 
     private byte[] data;
     private int offset = 0;
+    boolean bigEndian = true;
+    boolean nativeByteOrder = (ByteOrder.nativeOrder() == ByteOrder.BIG_ENDIAN);
+
 
     protected ByteBuffer(byte[] arr) {
         this.data = arr;
@@ -13,8 +16,16 @@ public class ByteBuffer extends Buffer {
         return new ByteBuffer(new byte[capacity]);
     }
 
+    public static ByteBuffer allocateDirect(int capacity) {
+        return allocate(capacity);
+    }
+
     public static ByteBuffer wrap(byte[] arr) {
         return new ByteBuffer(arr);
+    }
+
+    public boolean isDirect() {
+        return true;
     }
 
     public boolean hasArray() {
@@ -30,4 +41,20 @@ public class ByteBuffer extends Buffer {
         return this.offset;
     }
 
+    @Override
+    public ByteBuffer rewind() {
+        super.rewind();
+        return this;
+    }
+
+    public final ByteOrder order() {
+        return bigEndian ? ByteOrder.BIG_ENDIAN : ByteOrder.LITTLE_ENDIAN;
+    }
+
+    public final ByteBuffer order(ByteOrder bo) {
+        bigEndian = (bo == ByteOrder.BIG_ENDIAN);
+        nativeByteOrder =
+            (bigEndian == (ByteOrder.nativeOrder() == ByteOrder.BIG_ENDIAN));
+        return this;
+    }
 }
